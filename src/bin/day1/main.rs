527cd21a5f1f8d8cbf11945201af962f18c88aa8
Ryan Hofer <r.h.hofer@gmail.com>
2018-12-01 01:16:54 -0600

    Extract iter parsing to fn

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
+use std::ops::Fn;
+use std::iter;
+use std::str;
 
 const INPUT: &'static str = include_str!("input.txt");
 
-fn get_lines() -> std::str::Split<'static, &'static str> {
-    INPUT.trim().split("\n")
+fn get_freq_changes<'a>() -> iter::Map<str::Split<'a, &'a str>, &'static Fn(&str) -> i64> {
+    INPUT
+        .trim()
+        .split("\n")
+        .map(&atoi)
+}
+
+fn atoi(s: &str) -> i64 {
+    s.parse().unwrap_or(0)
 }
 
 fn main() {
@@ -13,8 +23,7 @@ fn main() {
 
 fn one() -> i64 {
     let mut frequency: i64 = 0;
-    for line in get_lines() {
-        let delta: i64 = line.trim().parse().unwrap();
+    for delta in get_freq_changes() {
         frequency += delta;
     }
 
@@ -26,8 +35,7 @@ fn two() -> i64 {
     let mut frequency: i64 = 0;
     seen.insert(frequency);
 
-    for line in get_lines().cycle() {
-        let delta: i64 = line.trim().parse().unwrap();
+    for delta in get_freq_changes().cycle() {
         frequency += delta;
         if seen.contains(&frequency) {
             break;
