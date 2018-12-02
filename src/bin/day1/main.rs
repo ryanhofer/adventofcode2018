79571a0c79b417d02ca8696c6e441e691cb3c295
Ryan Hofer <r.h.hofer@gmail.com>
2018-12-01 21:54:34 -0600

    Refactor

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,41 +1,27 @@
 use std::collections::HashSet;
-use std::ops::Fn;
-use std::iter;
-use std::str;
 
 const INPUT: &'static str = include_str!("input.txt");
 
-fn get_freq_changes<'a>() -> iter::Map<str::Split<'a, &'a str>, &'static Fn(&str) -> i64> {
-    INPUT
-        .trim()
-        .split("\n")
-        .map(&atoi)
-}
-
-fn atoi(s: &str) -> i64 {
-    s.parse().unwrap_or(0)
-}
-
 fn main() {
-    println!("Part One: {}", one());
-    println!("Part Two: {}", two());
+    println!("Part One: {}", part_one());
+    println!("Part Two: {}", part_two());
 }
 
-fn one() -> i64 {
+fn part_one() -> i64 {
     let mut frequency: i64 = 0;
-    for delta in get_freq_changes() {
+    for delta in INPUT.lines().map(atoi) {
         frequency += delta;
     }
 
     frequency
 }
 
-fn two() -> i64 {
+fn part_two() -> i64 {
     let mut seen = HashSet::new();
     let mut frequency: i64 = 0;
     seen.insert(frequency);
 
-    for delta in get_freq_changes().cycle() {
+    for delta in INPUT.lines().map(atoi).cycle() {
         frequency += delta;
         if seen.contains(&frequency) {
             break;
@@ -44,4 +30,8 @@ fn two() -> i64 {
     }
 
     frequency
+}
+
+fn atoi(s: &str) -> i64 {
+    s.parse().unwrap_or(0)
 }
